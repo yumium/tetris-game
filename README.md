34cc0d4ce9e9773d72c31cf9f83d9f944c454c50
yumium <yuming.zai.00@gmail.com>
2023-08-31 04:34:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Tetris Game Simulator
+A simulator for Tetris that takes in a comma separated list of Tetris pieces and positions and return the height of the final board.
+
+The pieces take the following shape:
+
+![tetris-pieces](.\static\tetris-pieces.png)
+
+ An example input could be
+
+> Q0,I2,I6,I0,I6,I6,Q2,Q4
+
+Meaning in the game we first add shape T where its left column is positioned at column with index 0, then adding shape Z where its left column is positioned at column with index 2, and so on ...
+
+Here, the board has 10 columns. There is no height limit.
+
+After the first three pieces drop, the result is as follows:
+
+![step-1](.\static\step-1.png)
+
+The bottom line is cleared, and after the next five pieces drop, here is the result:
+
+![step-2](.\static\step-2.png)
+
+The second line clears, and the final result is as follows:
+
+![step-3](.\static\step-3.png)
+
+Hence, the simulator returns height "3".
+
+
+
+
+
+### Example usage
+Running in command line
+```bash
+$ ./tetris < input.txt > output.txt
+```
+
+Where `input.txt` contains sequences of pieces inputs:
+```
+
+Q0
+Q0,Q2,Q4,Q6,Q8,Q1
+Q0,I2,I6,I0,I6,I6,Q2,Q4
+```
+
+And `output.txt` should contain the corresponding resulting heights:
+```
+0
+2
+2
+3
+```
+
+
+
+### Running tests
+
+To run the unit tests you will have to install `pytest`. Then simply:
+```shell
+cd tests
+python -m pytest .\test_tetris.py
+```
+
+To visualise the test coverage you will have to additionally install `pytest-cov`. Then simply: 
+```shell
+py.test --cov tetris --cov-report html --cov-branch .\test_tetris.py
+```
+
---
+++ tests/test_tetris.py
@@ -0,0 +1,203 @@
+# PyTest file
+import sys
+sys.path.append('../')
+from tetris import TetrisGame, DEFAULT_WIDTH, parse_sequence, simulate_game
+import pytest
+
+def test_add_empty():
+    # setup
+    board = []
+    heights = [0]*DEFAULT_WIDTH
+    tetris = TetrisGame()
+
+    # execute
+    res = tetris.add('Q', 0, board, heights=heights)
+
+    # assert
+    assert res == [
+        [1,1,0,0,0,0,0,0,0,0],
+        [1,1,0,0,0,0,0,0,0,0]
+    ]
+
+def test_add_stack():
+    # setup
+    board = [
+        [1,1,0,0,0,0,0,0,0,0],
+        [1,1,0,0,0,0,0,0,0,0]
+    ]
+    heights = [2,2,0,0,0,0,0,0,0,0]
+    tetris = TetrisGame()
+
+    # execute
+    res = tetris.add('Q', 1, board, heights=heights)
+    print(res)
+
+    # assert
+    assert res == [
+        [0,1,1,0,0,0,0,0,0,0],
+        [0,1,1,0,0,0,0,0,0,0],
+        [1,1,0,0,0,0,0,0,0,0],
+        [1,1,0,0,0,0,0,0,0,0]
+    ]
+
+def test_add_clear_line():
+    # setup
+    board = [
+        [1,1,1,1,1,1,1,1,0,0],
+        [1,1,1,1,1,1,1,1,0,0]
+    ]
+    heights = [2,2,2,2,2,2,2,2,0,0]
+    tetris = TetrisGame()
+
+    # execute
+    res = tetris.add('J', 8, board, heights=heights)
+
+    # assert
+    assert res == [
+        [0,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,0,1]
+    ]
+
+def test_add_clear_line_multiple():
+    # setup
+    board = [
+        [1,1,1,1,1,1,1,1,0,0],
+        [1,1,1,1,1,1,1,1,0,0]
+    ]
+    heights = [2,2,2,2,2,2,2,2,0,0]
+    tetris = TetrisGame()
+
+    # execute
+    res = tetris.add('Q', 8, board, heights=heights)
+
+    # assert
+    assert res == []
+
+def test_get_height_empty():
+    # setup
+    tetris = TetrisGame()
+    board = []
+
+    # execute
+    res = tetris.get_height(board)
+
+    # assert
+    assert res == 0
+
+def test_get_height_single():
+    # setup
+    tetris = TetrisGame()
+    board = [
+        [1,1,0,0,0,0,0,0,0,0]
+    ]
+
+    # execute
+    res = tetris.get_height(board)
+
+    # assert
+    assert res == 1
+
+def test_get_height_multiple():
+    # setup
+    tetris = TetrisGame()
+    board = [
+        [1,1,0,0,0,0,0,0,0,0],
+        [1,1,0,0,0,0,0,0,0,0]
+    ]
+
+    # execute
+    res = tetris.get_height(board)
+
+    # assert
+    assert res == 2
+
+def test__lock_empty():
+    # setup
+    tetris = TetrisGame()
+    heights = [0]*DEFAULT_WIDTH
+    piece = [
+        [1,1],
+        [1,1]
+    ]
+
+    # execute
+    res = tetris._lock(piece, 0, heights)
+
+    # assert
+    assert res == 1
+
+def test__lock_stack():
+    # setup
+    tetris = TetrisGame()
+    heights = [1,1,0,0,0,0,0,0,0,0]
+    piece = [
+        [0,1,1],
+        [1,1,0]
+    ]
+
+    # execute
+    res = tetris._lock(piece, 1, heights)
+
+    # assert
+    assert res == 2
+
+def test__lock_fit():
+    # setup
+    tetris = TetrisGame()
+    heights = [2,1,2,0,0,0,0,0,0,0]
+    piece = [
+        [1,1,1],
+        [0,1,0]
+    ]
+
+    # execute
+    res = tetris._lock(piece, 0, heights)
+
+    # assert
+    assert res == 2
+
+def test_parse_sequence_empty():
+    # setup
+    seq = ''
+
+    # execute
+    res = parse_sequence(seq)
+
+    # assert
+    assert res == []
+
+def test_parse_sequence_single():
+    # setup
+    seq = 'Q0'
+
+    # execute
+    res = parse_sequence(seq)
+
+    # assert
+    assert res == [('Q',0)]
+
+def test_parse_sequence_multiple():
+    # setup
+    seq = 'Q0,Q0,T2'
+
+    # execute
+    res = parse_sequence(seq)
+
+    # assert
+    assert res == [('Q',0), ('Q',0), ('T',2)]
+
+def load_test_cases():
+    with open('./test.in', 'r') as f:
+        return [l.strip() for l in f.readlines()]
+
+def load_answers():
+    with open('./test.ans', 'r') as f:
+        return [int(l.strip()) for l in f.readlines()]
+
+@pytest.mark.parametrize("sequence,ans", list(zip(load_test_cases(), load_answers())))
+def test_tetris_game(sequence, ans):
+    # execute
+    res = simulate_game(parse_sequence(sequence))
+
+    # assert
+    res == ans
